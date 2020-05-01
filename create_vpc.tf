51d929ba223533cb4bb39eed0965bcd27266b9f7
Ingmar Schoen <ingmar.schoen@gmail.com>
2020-05-01 23:27:06 +0200

    Add security group to VPC

--- create_vpc.tf
+++ create_vpc.tf
@@ -32,3 +32,44 @@ rosource "aws_route_table_association" "main" {
   subnet_id = aws_subnet.main.id
   route_table_id = aws_route_table.default.id
 }
+
+resource "aws_network_acl" "allowall" {
+  vpc_id = aws_vpc.main.id
+
+  egress {
+    protocol = "-1"
+    rule_no = 100
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 0
+    to_port = 0
+  }
+
+  ingress {
+    protocol = "-1"
+    rule_no = 200
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 0
+  }
+}
+
+resource "aws_security_group" "allowall" {
+  name = "TF VPC lab allow all"
+  description "Allow all traffic"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_block = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_block = ["0.0.0.0/0"]
+
+}
